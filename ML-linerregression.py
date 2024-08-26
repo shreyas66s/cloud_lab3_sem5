a24d0820509be24e59239de6959ddc06fe806ec7
shreyas66s <shreyas1665@gmail.com>
2024-08-26 21:51:49 +0530

    first commit

---
+++ ML-linerregression.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sklearn
+from sklearn import linear_model
+height=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0]]
+weight=[  8, 10 , 12, 14, 16, 18, 20]
+reg=linear_model.LinearRegression()
+reg.fit(height,weight)
+X_height=[[12.0]]
+print(reg.predict(X_height))
